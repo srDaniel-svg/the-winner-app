01e2115be8279ac37c94f3668ba89cab2620e5d1
kevinpro <kq47161@gmail.com>
2025-08-30 04:14:18 -0400

    Agrega la dependencia 'lucide-react-native' y actualiza la configuración de VSCode para el puerto de Live Server. Mejora el estilo de la pantalla de perfil con nuevos estilos y estructura.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,25 @@
-import {View, Text} from 'react-native'
+import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../../constants/Colors';
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.light.text,
+  },
+});

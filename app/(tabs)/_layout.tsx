4487bb10f11c914a12fac31d6b23553c4ca42cd5
Andres Eduardo Rosas Alpiri <alpiryk@gmail.com>
2025-08-30 03:31:23 -0400

    Refactor tab layout and screens; remove unused components and implement new structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,25 +2,16 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { House } from 'lucide-react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -30,14 +21,28 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <House size={28} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="reload"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Recargar',
+          tabBarIcon: ({ color }) => <House size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <House size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <House size={28} color={color} />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/reload.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function ReloadScreen() {
+  return (
+    <View>
+      <Text>Reload</Text>
+    </View>
+  );
+}
